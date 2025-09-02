c3781ecc0c0a7e1c6df96903f9ca05e63b5b2d30
Leila Noval <109560883+lenovalsha@users.noreply.github.com>
2025-09-01 20:40:29 -0600

    Created the CRUD Methods

--- PracticingCRUDSQLite/MainWindow.xaml.cs
+++ PracticingCRUDSQLite/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace PracticingCRUDSQLite
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<User> DatabaseUsers;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +29,21 @@ namespace PracticingCRUDSQLite
 
         private void btnCreate_Click(object sender, RoutedEventArgs e)
         {
+            Create();
+        }
+       
+        private void btnRead_Click(object sender, RoutedEventArgs e)
+        {
+            Read();
+        }
 
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            Update();
+        }
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Delete();
         }
         public void Create()
         {
@@ -35,27 +51,60 @@ namespace PracticingCRUDSQLite
             {
                 var name = txtName.Text;
                 var address = txtAddress.Text;
-                if (name != null && address != null)
+                if (name != string.Empty && address != string.Empty)
                 {
                     context.Users.Add(new User() { Name = name, Address = address });
                     context.SaveChanges();
                 }
-                else
-                    return;
 
             }
         }
         public void Read()
         {
-
+            using (DataContext context = new DataContext())
+            {
+                DatabaseUsers = context.Users.ToList();
+                UserList.ItemsSource = DatabaseUsers;
+            }
         }
         public void Update()
         {
+            using (DataContext context = new DataContext())
+            {
+                User selectedUser = UserList.SelectedItem as User;//will select the user item and convert to user type - this way we can get the Id
+                var name = txtName.Text;
+                var address = txtAddress.Text;
+                if (name != string.Empty && address != string.Empty)
+                {
 
+                    User user = context.Users.Find(selectedUser.Id);//find the user based on the Id property
+                    user.Name = name;
+                    user.Address = address;
+
+                    context.SaveChanges();
+                }
+
+                //this just updates our listview
+                DatabaseUsers = context.Users.ToList();
+                UserList.ItemsSource = DatabaseUsers;
+            }
         }
         public void Delete()
         {
+            using (DataContext context = new DataContext())
+            {
+                User selectedUser = UserList.SelectedItem as User;//will select the user item and convert to user type - this way we can get the Id
+                User user = context.Users.Find(selectedUser.Id);//find the user based on the Id property
 
+                context.Users.Remove(user);
+                context.SaveChanges();
+
+
+                //this just updates our listview
+                DatabaseUsers = context.Users.ToList();
+                UserList.ItemsSource = DatabaseUsers;
+            }
         }
+
     }
 }
